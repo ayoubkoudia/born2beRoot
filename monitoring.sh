a9947157a1d97d9dd61068d68d2a2714f8feb608
Dazendust <93022275+ayoubkoudia@users.noreply.github.com>
2022-11-22 16:58:17 +0100

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -1,31 +1,30 @@
-#!/bin/bash
-arc=$(uname -a)
-pcpu=$(grep "physical id" /proc/cpuinfo | sort | uniq | wc -l)
-vcpu=$(grep "^processor" /proc/cpuinfo | wc -l)
-fram=$(free -m | awk '$1 == "Mem:" {print $2}')
-uram=$(free -m | awk '$1 == "Mem:" {print $3}')
-pram=$(free | awk '$1 == "Mem:" {printf("%.2f"), $3/$2*100}')
-fdisk=$(df -Bg | grep '^/dev/' | grep -v '/boot$' | awk '{ft += $2} END {print ft}')
-udisk=$(df -Bm | grep '^/dev/' | grep -v '/boot$' | awk '{ut += $3} END {print ut}')
-pdisk=$(df -Bm | grep '^/dev/' | grep -v '/boot$' | awk '{ut += $3} {ft+= $2} END {printf("%d"), ut/ft*100}')
-cpul=$(top -bn1 | grep '^%Cpu' | cut -c 9- | xargs | awk '{printf("%.1f%%"), $1 + $3}')
-lb=$(who -b | awk '$1 == "system" {print $3 " " $4}')
-lvmt=$(lsblk | grep "lvm" | wc -l)
-lvmu=$(if [ $lvmt -eq 0 ]; then echo no; else echo yes; fi)
-ctcp=$(cat /proc/net/sockstat{,6} | awk '$1 == "TCP:" {print $3}')
-ulog=$(users | wc -w)
-ip=$(hostname -I)
-mac=$(ip link show | awk '$1 == "link/ether" {print $2}')
-cmds=$(journalctl _COMM=sudo | grep COMMAND | wc -l)
-wall "	#Architecture: $arc
-	#CPU physical: $pcpu
-	#vCPU: $vcpu
-	#Memory Usage: $uram/${fram}MB ($pram%)
-	#Disk Usage: $udisk/${fdisk}Gb ($pdisk%)
-	#CPU load: $cpul
-	#Last boot: $lb
-	#LVM use: $lvmu
-	#Connexions TCP: $ctcp ESTABLISHED
-	#User log: $ulog
-	#Network: IP $ip ($mac)
-	#Sudo: $cmds cmd"
+#! /bin/bash
+
+if lvdisplay > /dev/null 2>&1; then
+	lvm_usage="no"
+else
+	lvm_usage="yes"
+fi
+
+mem_total=$(free --mega | grep Mem: | tr -s ' ' | cut -d ' ' -f 2)
+mem_used=$(free --mega | grep Mem: | tr -s ' ' | cut -d ' ' -f 3)
+mem_pcent=$((mem_used * 100 / mem_total))
+
+disk_used=$(df -h --total | grep total | awk '{print $3}')
+disk_total=$(df -h --total | grep total | awk '{print $4}')
+disk_pcent=$(df -h --total | grep total | awk '{print $5}')
+
+sudo_line=$(cat /var/log/sudo/sudo.log | wc -l)
+
+wall "	#Architecture : $(uname -a)
+	#CPU physical : $(nproc)
+	#vCPU : $(grep processor /proc/cpuinfo | wc -l)
+	#Memory Usage : $mem_used/$mem_total MB ($mem_pcent%)
+	#Disk Usage : $disk_used/$disk_total ($disk_pcent)
+	#CPU load : $(mpstat | grep all | awk '{print 100-$13}')%
+	#Last boot : $(who -b | awk '{print $3,$4}')
+	#LVM use : $lvm_usage
+	#Connexions TCP : $(netstat -at | grep ESTABLISHED | grep 4242 | wc -l)
+	#User log : $(who | wc -l)
+	#Network : IP $(hostname -I)($(ip a | grep ether | awk '{print $2}'))
+	#Sudo : $((sudo_line / 2)) cmd"
